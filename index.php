f4befb303bd22435fe1b5b44e5e4adaf9c9c6295
daro29 <44953294+daro29@users.noreply.github.com>
2020-02-21 01:18:51 -0600

    Estructura del blog
    inicio

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML>
+<html lang='es'>
+<head>
+        <meta charset = "utf-8"/>
+        <title>BLOG - Videojuegos</title>
+ </head>
+ <body>
+        <!-- CABECERA -->
+        <header id="header">
+                <!-- Logo -->
+                <div class="logo">
+                    <a href="index.php">
+                        Blog de videojuegos
+                     </a>
+                </div>
+            <!-- Menu -->    
+            <nav id=nav>
+                <ul>
+                    <li>
+                        <a href="index.php">Inicio</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Categoria 1</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Categoria 2</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Categoria 3</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Categoria 4</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Sobre Mi</a>
+                    </li>
+                    <li>
+                        <a href="index.php">Contacto</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+       
+  
+            <!-- Barra lateral -->
+            <!-- Conternido pincipal -->
+ </body>
+     <footer>
+    </footer>
+</html>
